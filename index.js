8e5eeddeaff2be94c53a53d18af7d2c66b4e3c0a
reesecantu <65140498+reesecantu@users.noreply.github.com>
2026-03-31 16:52:43 -0400

    5.4.5 implementation

--- index.js
+++ index.js
@@ -4,11 +4,34 @@ import Lab5 from "./Lab5/index.js";
 import cors from "cors";
 import db from "./Kambaz/Database/index.js";
 import UserRoutes from "./Kambaz/users/routes.js";
-
+import CourseRoutes from "./Kambaz/courses/routes.js";
+import "dotenv/config";
+import session from "express-session";
 const app = express();
-app.use(cors());
+app.use(
+  cors({
+    credentials: true,
+    origin: process.env.CLIENT_URL || "http://localhost:3000",
+  })
+);
+const sessionOptions = {
+  secret: process.env.SESSION_SECRET || "kambaz",
+  resave: false,
+  saveUninitialized: false,
+};
+if (process.env.SERVER_ENV !== "development") {
+  sessionOptions.proxy = true;
+  sessionOptions.cookie = {
+    sameSite: "none",
+    secure: true,
+    domain: process.env.SERVER_URL,
+  };
+}
+app.use(session(sessionOptions));
+
 app.use(express.json());
 UserRoutes(app, db);
+CourseRoutes(app, db);
 Lab5(app);
 Hello(app);
 app.listen(process.env.PORT || 4000);
