38664d6108b9c576f7d27ecf1bbed4c3fc891a78
reesecantu <65140498+reesecantu@users.noreply.github.com>
2026-04-01 18:10:07 -0400

    5.4.6 + 5.4.7 implementation

---
+++ Kambaz/assignments/dao.js
@@ -0,0 +1,28 @@
+import { v4 as uuidv4 } from "uuid";
+export default function AssignmentsDao(db) {
+  function findAssignmentsForCourse(courseId) {
+    const { assignments } = db;
+    return assignments.filter((assignment) => assignment.course === courseId);
+  }
+  function createAssignment(assignment) {
+    const newAssignment = { ...assignment, _id: uuidv4() };
+    db.assignments = [...db.assignments, newAssignment];
+    return newAssignment;
+  }
+  function deleteAssignment(assignmentId) {
+    const { assignments } = db;
+    db.assignments = assignments.filter((a) => a._id !== assignmentId);
+  }
+  function updateAssignment(assignmentId, assignmentUpdates) {
+    const { assignments } = db;
+    const assignment = assignments.find((a) => a._id === assignmentId);
+    Object.assign(assignment, assignmentUpdates);
+    return assignment;
+  }
+  return {
+    findAssignmentsForCourse,
+    createAssignment,
+    deleteAssignment,
+    updateAssignment,
+  };
+}
