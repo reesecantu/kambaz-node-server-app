ba2ce2a49cf2160e4bb510c4d5fbd806318d6ee8
reesecantu <65140498+reesecantu@users.noreply.github.com>
2026-04-11 20:39:54 -0400

    work on deploying to server

--- Kambaz/users/dao.js
+++ Kambaz/users/dao.js
@@ -1,12 +1,21 @@
+import { v4 as uuidv4 } from "uuid";
 import model from "./model.js";
 
 export default function UsersDao() {
-  const createUser = (user) => {} // implemented later
+  const createUser = (user) => {
+    // had some issues with uuid4 not beind defined
+    const { _id, ...userWithoutId } = user;
+    const newUser = { ...userWithoutId, _id: uuidv4() };
+    return model.create(newUser);
+  };
   const findAllUsers = () => model.find();
   const findUserById = (userId) => model.findById(userId);
-  const findUserByUsername = (username) =>  model.findOne({ username: username });
-  const findUserByCredentials = (username, password) =>  model.findOne({ username, password });
-  const updateUser = (userId, user) =>  model.updateOne({ _id: userId }, { $set: user });
+  const findUserByUsername = (username) =>
+    model.findOne({ username: username });
+  const findUserByCredentials = (username, password) =>
+    model.findOne({ username, password });
+  const updateUser = (userId, user) =>
+    model.updateOne({ _id: userId }, { $set: user });
   const deleteUser = (userId) => model.findByIdAndDelete(userId);
   const findUsersByRole = (role) => model.find({ role: role });
   const findUsersByPartialName = (partialName) => {
@@ -15,5 +24,15 @@ export default function UsersDao() {
       $or: [{ firstName: { $regex: regex } }, { lastName: { $regex: regex } }],
     });
   };
-  return { createUser, findAllUsers, findUserById, findUserByUsername, findUserByCredentials, updateUser, findUsersByRole, findUsersByPartialName, deleteUser };
+  return {
+    createUser,
+    findAllUsers,
+    findUserById,
+    findUserByUsername,
+    findUserByCredentials,
+    updateUser,
+    findUsersByRole,
+    findUsersByPartialName,
+    deleteUser,
+  };
 }
--- Kambaz/users/routes.js
+++ Kambaz/users/routes.js
@@ -1,7 +1,10 @@
 import UsersDao from "./dao.js";
 export default function UserRoutes(app) {
   const dao = UsersDao();
-  const createUser = (req, res) => {};
+  const createUser = async (req, res) => {
+    const user = await dao.createUser(req.body);
+    res.json(user);
+  };
   const deleteUser = async (req, res) => {
     const status = await dao.deleteUser(req.params.userId);
     res.json(status);
@@ -21,7 +24,6 @@ export default function UserRoutes(app) {
     const users = await dao.findAllUsers();
     res.json(users);
   };
-
   const findUserById = async (req, res) => {
     const user = await dao.findUserById(req.params.userId);
     res.json(user);
@@ -36,7 +38,6 @@ export default function UserRoutes(app) {
     }
     res.json(currentUser);
   };
-
   const signin = async (req, res) => {
     const { username, password } = req.body;
     console.log("signin body:", username, password);
